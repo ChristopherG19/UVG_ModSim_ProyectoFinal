ff116bafb75332cb665fae78aaa27d0e6de6f646
Christopher García <60325784+ChristopherG19@users.noreply.github.com>
2023-11-14 01:45:28 -0600

    Aditional

--- 3D_representation/Rubiks_Cube3x3_V2.py
+++ 3D_representation/Rubiks_Cube3x3_V2.py
@@ -39,6 +39,30 @@ class Game:
         self.MIDDLE_X = {Vec3(0, y, z) for y in range(-1, 2) for z in range(-1, 2)}
         self.MIDDLE_Y = {Vec3(x, 0, z) for x in range(-1, 2) for z in range(-1, 2)}
         self.MIDDLE_Z = {Vec3(x, y, 0) for x in range(-1, 2) for y in range(-1, 2)}
+        
+        # # Botón para girar en X
+        # self.rotate_X_button = Button(text="X", color=button_color, scale=button_scale, position=(0.7, 0.2 + 3 * button_spacing))
+        # self.rotate_X_button.on_click = self.rotate_X
+
+        # self.rotate_anti_X_button = Button(text="Xi", color=button_color, scale=button_scale, position=(0.77, 0.2 + 3 * button_spacing))
+        # self.rotate_anti_X_button.on_click = self.rotate_X_anti
+        
+        # # Botón para girar en Y
+        # self.rotate_Y_button = Button(text="Y", color=button_color, scale=button_scale, position=(0.7, 0.2 + 2 * button_spacing))
+        # self.rotate_Y_button.on_click = self.rotate_Y
+
+        # self.rotate_anti_Y_button = Button(text="Yi", color=button_color, scale=button_scale, position=(0.77, 0.2 + 2 * button_spacing))
+        # self.rotate_anti_Y_button.on_click = self.rotate_Y_anti
+        
+        # # Botón para girar en Z
+        # self.rotate_Z_button = Button(text="Z", color=button_color, scale=button_scale, position=(0.7, 0.2 + button_spacing))
+        # self.rotate_Z_button.on_click = self.rotate_Z
+
+        # self.rotate_anti_Z_button = Button(text="Zi", color=button_color, scale=button_scale, position=(0.77, 0.2 + button_spacing))
+        # self.rotate_anti_Z_button.on_click = self.rotate_Z_anti
+        
+        # self.rotate_right_button_anti = Button(text="Ri", color=button_color, scale=button_scale, position=(0.77, 0.2))
+        # self.rotate_right_button_anti.on_click = self.rotate_right_face_anti
 
         # Botón para rotar la cara derecha
         self.rotate_right_button = Button(text="R", color=button_color, scale=button_scale, position=(0.7, 0.2))
@@ -157,7 +181,8 @@ class Game:
   
     def shuffle_cube(self):
         # Barajar el cubo realizando movimientos aleatorios con retraso
-        possible_moves = ['LEFT', 'RIGHT', 'TOP', 'BOTTOM', 'FRONT', 'BACK', 'MIDDLE_X', 'MIDDLE_Y', 'MIDDLE_Z']
+        possible_moves = ['LEFT', 'RIGHT', 'TOP', 'BOTTOM', 'FRONT', 'BACK', 'MIDDLE_X', 'MIDDLE_Y', 'MIDDLE_Z',
+                          'LEFTi', 'RIGHTi', 'TOPi', 'BOTTOMi', 'FRONTi', 'BACKi', 'MIDDLE_Xi', 'MIDDLE_Yi', 'MIDDLE_Zi']
         num_moves = 20  # Puedes ajustar la cantidad de movimientos aleatorios
         delay_between_moves = 0.5  # Ajusta el retraso entre movimientos
         
@@ -173,8 +198,6 @@ class Game:
                 self.update_move_text()
 
         shuffle_recursive()
-        
-        self.C = random_input(self.movimientos_show)
 
     def rotate_right_face(self):
         self.rotate_side('RIGHT')
@@ -249,25 +272,25 @@ class Game:
         self.update_move_text()
 
     def rotate_middle_x_layer(self):
-        self.rotate_side('MIDDLE_X')
+        self.rotate_side_2('MIDDLE_X')
         self.movimientos.append('MIDDLE_X')
         self.movimientos_show.append(self.to_rubik_notation('MIDDLE_X'))
         self.update_move_text()
         
     def rotate_middle_x_layer_anti(self):
-        self.rotate_side_2('MIDDLE_Xi')
+        self.rotate_side('MIDDLE_Xi')
         self.movimientos.append('MIDDLE_Xi')
         self.movimientos_show.append(self.to_rubik_notation('MIDDLE_Xi'))
         self.update_move_text()
 
     def rotate_middle_y_layer(self):
-        self.rotate_side('MIDDLE_Y')
+        self.rotate_side_2('MIDDLE_Y')
         self.movimientos.append('MIDDLE_Y')
         self.movimientos_show.append(self.to_rubik_notation('MIDDLE_Y'))
         self.update_move_text()
 
     def rotate_middle_y_layer_anti(self):
-        self.rotate_side_2('MIDDLE_Yi')
+        self.rotate_side('MIDDLE_Yi')
         self.movimientos.append('MIDDLE_Yi')
         self.movimientos_show.append(self.to_rubik_notation('MIDDLE_Yi'))
         self.update_move_text()
@@ -282,65 +305,170 @@ class Game:
         self.rotate_side_2('MIDDLE_Zi')
         self.movimientos.append('MIDDLE_Zi')
         self.movimientos_show.append(self.to_rubik_notation('MIDDLE_Zi'))
-        self.update_move_text()        
-    
+        self.update_move_text()    
+        
+    def rotate_X(self):
+        moves = ['Li', 'M', 'R']
+        self.movimientos.append("LEFT")
+        self.movimientos.append("MIDDLE_X")
+        self.movimientos.append("RIGHT")
+        self.movimientos_show.append('X')
+        self.update_move_text()
+        
+        def rotate_layers():
+            if moves:
+                movement = moves.pop(0)
+                self.rotate_side(movement)
+            invoke(rotate_layers, delay=0.50)
+            
+        rotate_layers()
+            
+    def rotate_X_anti(self):    
+        moves = ['L', 'Mi', 'Ri']
+        self.movimientos.append("LEFTi")
+        self.movimientos.append("MIDDLE_Xi")
+        self.movimientos.append("RIGHTi")
+        self.movimientos_show.append('Xi')
+        self.update_move_text()
+        
+        def rotate_layers():
+            if moves:
+                movement = moves.pop(0)
+                self.rotate_side_2(movement)
+            invoke(rotate_layers, delay=0.50)
+            
+        rotate_layers()
+        
+    def rotate_Y(self):
+        moves = ['Ui', 'E', 'D']
+        self.movimientos.append("TOP")
+        self.movimientos.append("MIDDLE_Y")
+        self.movimientos.append("BOTTOM")
+        self.movimientos_show.append('Y')
+        self.update_move_text()
+        
+        def rotate_layers():
+            if moves:
+                movement = moves.pop(0)
+                self.rotate_side(movement)
+            invoke(rotate_layers, delay=0.50)
+            
+        rotate_layers()
+            
+    def rotate_Y_anti(self): 
+        moves = ['U', 'Ei', 'Di']
+        self.movimientos.append("TOP")
+        self.movimientos.append("MIDDLE_Yi")
+        self.movimientos.append("BOTTOMi")
+        self.movimientos_show.append('Yi')
+        self.update_move_text()
+        
+        def rotate_layers():
+            if moves:
+                movement = moves.pop(0)
+                self.rotate_side_2(movement)
+            invoke(rotate_layers, delay=0.50)
+            
+        rotate_layers()
+        
+    def rotate_Z(self):
+        moves = ['Bi', 'S', 'F']
+        self.movimientos.append("BACK")
+        self.movimientos.append("MIDDLE_Z")
+        self.movimientos.append("FRONT")
+        self.movimientos_show.append('Z')
+        self.update_move_text()
+        
+        def rotate_layers():
+            if moves:
+                movement = moves.pop(0)
+                self.rotate_side(movement)
+            invoke(rotate_layers, delay=0.50)
+            
+        rotate_layers()
+            
+    def rotate_Z_anti(self):  
+        moves = ['B', 'Si', 'Fi']
+        self.movimientos.append("BACKi")
+        self.movimientos.append("MIDDLE_Zi")
+        self.movimientos.append("FRONTi")
+        self.movimientos_show.append('Zi')
+        self.update_move_text()
+        
+        def rotate_layers():
+            if moves:
+                movement = moves.pop(0)
+                self.rotate_side_2(movement)
+            invoke(rotate_layers, delay=0.50)
+            
+        rotate_layers()
+
     def rotate_to_solve(self):
-        
-        if(self.C == None):
-            self.C = random_input([self.to_rubik_notation(x) for x in self.movimientos])
-        
-        solver = Solver(self.C)
-
-        start = time.time()
-        solver.solve()
-        duration = time.time() - start
-
-        movements = solver.moves
-        delay_between_moves = 0.6
+        reverse_movements = self.movimientos[::-1]
+        delay_between_moves = self.animation_time + random.uniform(0.5, 1.5)
         # delay_between_moves = self.animation_time + 0.11  # Delay de la función rotate_side_2
 
         def solve_recursive():
-            if movements:
-                movement = movements.pop(0)
-                if(movement[0] in 'XYZ'):
-                    if(movement[0] == 'X'):
-                        if(movement[-1] == 'i'):
-                            self.rotate_side_2('Li')
-                            self.rotate_side_2('Mi')
-                            self.rotate_side_2('Ri')
-                        else:
-                            self.rotate_side('L')
-                            self.rotate_side('M')
-                            self.rotate_side('R')
-                    elif(movement[0] == 'Y'):
-                        if(movement[-1] == 'i'):
-                            self.rotate_side_2('Ui')
-                            self.rotate_side_2('Ei')
-                            self.rotate_side_2('Di')
-                        else:
-                            self.rotate_side('U')
-                            self.rotate_side('E')
-                            self.rotate_side('D')
-                    elif(movement[0] == 'Z'):
-                        if(movement[-1] == 'i'):
-                            self.rotate_side_2('Bi')
-                            self.rotate_side_2('Si')
-                            self.rotate_side_2('Fi')
-                        else:
-                            self.rotate_side('B')
-                            self.rotate_side('S')
-                            self.rotate_side('F')
-                else:
-                    if(movement[-1] != 'i'):
-                        self.rotate_side(movement)
-                    else:
-                        self.rotate_side_2(movement)
+            if reverse_movements:
+                movement = reverse_movements.pop(0)
+                self.rotate_side_2(movement)
                 invoke(solve_recursive, delay=delay_between_moves)
 
         solve_recursive()
         self.movimientos = []
         self.movimientos_show = []
         self.move_text.text = ""
+    
+    # def rotate_to_solve(self):
+        
+    #     if(self.movimientos_show):
+    #         self.C = random_input(self.movimientos_show)
+        
+    #     if(self.C == None):
+    #         self.C = random_input([self.to_rubik_notation(x) for x in self.movimientos])
+        
+    #     solver = Solver(self.C)
+
+    #     start = time.time()
+    #     solver.solve()
+    #     duration = time.time() - start
+
+    #     movements = solver.moves
+    #     delay_between_moves = 1.5
+    #     # delay_between_moves = self.animation_time + 0.11  # Delay de la función rotate_side_2
+
+    #     def solve_recursive():
+    #         if movements:
+    #             movement = movements.pop(0)
+    #             if(movement[0] in 'XYZ'):
+    #                 if(movement[0] == 'X'):
+    #                     if(movement[-1] == 'i'):
+    #                         self.rotate_X_anti()
+    #                     else:
+    #                         self.rotate_X()
+    #                 elif(movement[0] == 'Y'):
+    #                     if(movement[-1] == 'i'):
+    #                         self.rotate_Y_anti()
+    #                     else:
+    #                         self.rotate_Y()
+    #                 elif(movement[0] == 'Z'):
+    #                     if(movement[-1] == 'i'):
+    #                         self.rotate_Z_anti()
+    #                     else:
+    #                         self.rotate_Z()
+    #             else:
+    #                 if(movement[-1] != 'i'):
+    #                     self.rotate_side(movement)
+    #                 else:
+    #                     self.rotate_side_2(movement)
+    #             invoke(solve_recursive, delay=delay_between_moves)
+
+    #     solve_recursive()
+    #     self.movimientos = []
+    #     self.movimientos_show = []
+    #     self.move_text.text = ""
+        
+    #     print(f"{len(solver.moves)} moves in {duration}: {' '.join(solver.moves)}")
             
     def load_game(self):
         self.create_cube_positions()
@@ -348,7 +476,7 @@ class Game:
         self.PARENT = Entity()
         self.rotation_axes = {'LEFT': 'x', 'RIGHT': 'x', 'TOP': 'y', 'BOTTOM': 'y', 'FRONT': 'z', 'BACK': 'z', 'MIDDLE_X': 'x', 'MIDDLE_Y': 'y', 'MIDDLE_Z': 'z'} 
         self.cubes_side_positons = {'LEFT': self.LEFT, 'BOTTOM': self.BOTTOM, 'RIGHT': self.RIGHT, 'FRONT': self.FRONT, 'BACK': self.BACK, 'TOP': self.TOP, 'MIDDLE_X': self.MIDDLE_X, 'MIDDLE_Y': self.MIDDLE_Y, 'MIDDLE_Z': self.MIDDLE_Z}  # Incluye las capas internas
-        self.animation_time = 0.35
+        self.animation_time = 0.30
         self.action_trigger = True
         self.action_mode = True
         self.message = Text(origin=(0, 19), color=color.black)
@@ -404,7 +532,7 @@ class Game:
             if cube.position in cube_positions:
                 cube.parent = self.PARENT
                 eval(f'self.PARENT.animate_rotation_{rotation_axis}(90, duration=self.animation_time)')
-        invoke(self.toggle_animation_trigger, delay=self.animation_time + 0.11)
+        invoke(self.toggle_animation_trigger, delay=0.35)
         
     def rotate_side_2(self, side_name):
         side_name = self.from_rubik_notation(side_name)
@@ -418,7 +546,7 @@ class Game:
             if cube.position in cube_positions:
                 cube.parent = self.PARENT
                 eval(f'self.PARENT.animate_rotation_{rotation_axis}(-90, duration=self.animation_time)')
-        invoke(self.toggle_animation_trigger, delay=self.animation_time + 0.11)
+        invoke(self.toggle_animation_trigger, delay=0.35)
 
     def reparent_to_scene(self):
         for cube in self.CUBES:
